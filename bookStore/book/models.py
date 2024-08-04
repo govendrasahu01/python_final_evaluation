e31e0c72009ad46187c8599feb9d0b86c1808035
Govendra Sahu <72web.developer@gmail.com>
2024-08-04 16:28:53 +0530

    models are created

--- bookStore/book/models.py
+++ bookStore/book/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    biography = models.TextField()
+    def __str__(self) -> str:
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.IntegerField()
+    def __str__(self) -> str:
+        return f'{self.name} - {self.code}'
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    price = models.DecimalField(max_digits=5, decimal_places=3)
+    stock = models.IntegerField()
+    country = models.ManyToManyField(Country)
+    
+    def __str__(self):
+        return f'{self.name} - {self.author.name}'
+    
+
+
+    
